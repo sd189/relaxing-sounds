28c0bf19462a57faf020aadc636f673b4df0bdb7
Salah Aldin Alsasa <salahalsasa@gmail.com>
2020-02-05 16:55:55 +0300

    add error messages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'error.not_found' => ':entity not found',
+    'error.forbidden' => 'forbidden',
+    'error.auth' => 'Forbiden',
+    'error.notSpecified' => 'Not Specified',
+    'error.fieldNotExist' => 'Field Not Exist',
+];
