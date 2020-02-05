903f3f17a68fa05daa7935a3de6e1ea5f7ff7430
Salah Aldin Alsasa <salahalsasa@gmail.com>
2020-02-05 16:53:50 +0300

    add category api controller

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Validator;
+use App\Service\CategoryService;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+/**
+ * Class CategoryController
+ *
+ * @resource Categories
+ *
+ * @package App\Http\Controllers
+ */
+class CategoryController extends Controller
+{
+    private $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
+    public function getCategories(Request $request)
+    {
+        return $this->categoryService->getAll($request->all());
+    }
+
+    /*
+     * @Route("/{slug}", requirements={"slug"})
+     *
+     * @Method({"GET"})
+     */
+    public function getCategory($slug)
+    {
+        $category = $this->categoryService->getCategory($slug);
+
+        if (!$category) {
+            $this->throwNotFoundEntity(trans_choice('messages.error.not_found',0, ['entity' => trans_choice('messages.entity.category', 0)]));
+        }
+
+        return $category;
+    }
+}
