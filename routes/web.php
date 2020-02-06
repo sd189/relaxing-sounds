1153b825b8a4aaccc9d22c20298b16a28ae7773e
Salah Aldin Alsasa <salahalsasa@gmail.com>
2020-02-06 12:13:22 +0300

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'library'], function () {
+    Route::get('', 'CategoryController@viewAll')->name('categories');
+    Route::get('/{slug}', 'CategoryController@view')
+        ->where('slug', '[A-Za-z-0-9-]+')
+        ->name('category');
 });
+
+Route::any('{any}', function () {
+    return redirect(route('categories'));
+})->where('any', '.*');
