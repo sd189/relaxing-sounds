3810997956af09333c211f6a872578f87f4a3ced
Salah Aldin Alsasa <salahalsasa@gmail.com>
2020-02-06 12:12:58 +0300

    update categories seeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -12,8 +12,19 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         DB::table('categories')->insert([
-            'name' => 'rock',
-            'slug' => 'rock'
+            'name' => 'Bird Sounds',
+            'slug' => 'bird-sounds',
+            'image' => 'https://images.pexels.com/photos/416179/pexels-photo-416179.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Piano Sounds',
+            'slug' => 'piano-sounds',
+            'image' => 'https://www.thepiano.sg/sites/thepiano.sg/files/styles/read_article_main_picture/public/thepiano_images/read/2015/12/09/16/22/piano-keys.jpg?itok=4uyk29It'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Nature Sounds',
+            'slug' => 'nature-sounds',
+            'image' => 'https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
         ]);
     }
 }
